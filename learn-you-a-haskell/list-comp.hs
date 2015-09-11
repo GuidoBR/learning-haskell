d91750e748bcab0740f8fd041a643d7e48a1ae60
Guido <guidopercu@gmail.com>
2015-09-11 13:51:27 -0300

    Better List Comps

--- learn-you-a-haskell/list-comp.hs
+++ learn-you-a-haskell/list-comp.hs
@@ -1,3 +1,5 @@
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]   
 -- boomBangs [7..13]  
 -- ["BOOM!","BOOM!","BANG!","BANG!"]  
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]   
+length' xs = sum [1 | _ <- xs]   
