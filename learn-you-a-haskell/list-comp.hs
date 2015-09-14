a22776f410cf2ae4b0de940af25431c2167d59dc
Guido <guidopercu@gmail.com>
2015-09-13 22:48:04 -0300

    Listcomps with type declaration

--- learn-you-a-haskell/list-comp.hs
+++ learn-you-a-haskell/list-comp.hs
@@ -1,5 +1,8 @@
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]   
 -- boomBangs [7..13]  
 -- ["BOOM!","BOOM!","BANG!","BANG!"]  
+
+removeNonUppercase :: [Char] -> [Char]  
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]   
+
 length' xs = sum [1 | _ <- xs]   
