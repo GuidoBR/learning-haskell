ad838640029bdb8aaeaa16a6de1ea66500910b07
Guido <guidopercu@gmail.com>
2015-09-21 11:42:28 -0300

    Filter Array with Listcomp

---
+++ hacker-rank/fp-filter-array.hs
@@ -0,0 +1,10 @@
+filter' :: Int -> [Int] -> [Int]
+filter' n arr = [x | x <- arr, x < n]
+
+-- The Input/Output section. You do not need to change or modify this part
+main = do 
+    n <- readLn :: IO Int 
+    inputdata <- getContents 
+    let 
+        numbers = map read (lines inputdata) :: [Int] 
+    putStrLn . unlines $ (map show . filter' n) numbers
