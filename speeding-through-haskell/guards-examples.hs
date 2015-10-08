81f9b59c52e378521b71869921576f841d3b156e
Guido <guidopercu@gmail.com>
2015-10-08 11:21:26 -0300

    Guards Examples

--- speeding-through-haskell/guards-examples.hs
+++ speeding-through-haskell/guards-examples.hs
@@ -15,3 +15,14 @@ min' :: Ord a => a -> a -> a
 min' x y
     | x <= y    = x
     | otherwise = y
+
+drink :: (Fractional a, Ord a) => String -> a -> String
+drink sex n
+    | bac < soberLimit      = "You're sober"
+    | bac < drivingLimit    = "You can drive but it's a bad idea"
+    | bac < thinkingLimit   = "You can't think"
+    | otherwise             = "Quit drinking now!"
+    where   bac = n * if sex == "male" then 0.025 else 0.035
+            soberLimit      = 0.03
+            drivingLimit    = 0.08
+            thinkingLimit   = 0.10
