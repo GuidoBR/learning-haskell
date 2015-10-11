e03a6b6c639a2d7826c5c825baaf8eb0498a2df5
Guido <guidopercu@gmail.com>
2015-10-11 09:11:54 -0300

    Moar recursion!

--- learn-you-a-haskell/recursion.hs
+++ learn-you-a-haskell/recursion.hs
@@ -5,3 +5,8 @@ maximum' (x:xs)
     | x > maxTail = x  
     | otherwise = maxTail  
     where maxTail = maximum' xs 
+
+maximum2 :: (Ord a) => [a] => a
+maximum' []     = error "maximum of empty list"  
+maximum2 [x]    = x
+maximum2 (x:xs) = max x (maximum2 xs)
