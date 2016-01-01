a9567ad5e68b3c39fe51ce85d3d3bf377f272e76
Guido Luz Percú <guidopercu@gmail.com>
2016-01-01 21:29:33 -0200

    Hacker Rank 30 Days of Code - Day 1

---
+++ hacker-rank/30-days-of-code/day1-data-types.hs
@@ -0,0 +1,10 @@
+main = do
+        putStrLn "Primitive : double"
+        putStrLn "Primitive : char"
+        putStrLn "Primitive : boolean"
+        putStrLn "Primitive : int"
+        putStrLn "Referenced : String"
+        putStrLn "Primitive : boolean"
+        putStrLn "Primitive : double"
+        putStrLn "Primitive : char"
+        putStrLn "Referenced : String"
